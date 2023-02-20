431f099c59a0b53293713ec2437bfe7398ac9f4b
Farfama <farfama.hargaaya@gmail.com>
2023-02-20 17:49:53 +0900

    Update fetch leaderboard data

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import { GetStaticProps } from "next"
 
 
 
@@ -8,11 +9,23 @@ const Leaderboard: FC = () => {
   return (<>
     <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
       <h1 className="text-4xl font-bold">Leaderboard</h1>
+      <h1>Leaderboard</h1>
       <p>TODO: complete.</p>
       <p>All leaderboard entries should be links to user profile page.</p>
     </div>
   </>)
 
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3000/api/leaderboard");
+  const data = await response.json();
+  const leaderboard = data.leaderboard;
+  console.log(leaderboard);
+
+  return {
+    props: { leaderboard }
+  };
 }
 
 
