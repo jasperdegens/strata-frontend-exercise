3a96f0463c3f7e426bbef7aaecf5392e0c43cd58
Farfama <farfama.hargaaya@gmail.com>
2023-02-20 18:02:47 +0900

    Update render leaderboard data

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,21 +1,36 @@
 import { FC } from "react"
 import { GetStaticProps } from "next"
+import Link from "next/link"
+import Image from "next/image"
 
 
 
-const Leaderboard: FC = () => {
+const Leaderboard: FC<LeaderboardData> = ({leaderboard}) => {
 
 
   return (<>
     <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
       <h1 className="text-4xl font-bold">Leaderboard</h1>
-      <h1>Leaderboard</h1>
-      <p>TODO: complete.</p>
+      {leaderboard.map((item : UserDetails, index: number) => (
+      <Link href={`/leaderboard`} key={item.username}>
+        <div>
+          <Image
+            src={item.profileImage}
+            alt={`${item.username} profile image`}
+            className="rounded-full"
+            width={50}
+            height={50}
+          />
+        <p>{`${index + 1}. ${item.username}`}</p>
+        <p>{item.score}</p>
+        </div>
+      </Link>
+      ))}
       <p>All leaderboard entries should be links to user profile page.</p>
     </div>
-  </>)
-
-}
+  </>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch("http://localhost:3000/api/leaderboard");
