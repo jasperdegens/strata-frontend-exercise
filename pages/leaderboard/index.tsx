27f0427a92a8bff728ba8bdf64f743d90cf603a4
Nick Garbalau <garbalaunick@gmail.com>
2023-03-17 14:17:38 +0200

    Created Leaderboard page

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,19 +1,50 @@
-import { FC } from "react"
+'use client';
 
+import { LeaderboardData, UserDetails } from '../../types';
+import { Leader } from '../../components/Leader';
+import { checkEnvironment } from '../../helpers/checkEnvironment';
+import { useEffect, useState } from 'react';
 
-
-const Leaderboard: FC = () => {
-
-
-  return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">Leaderboard</h1>
-      <p>TODO: complete.</p>
-      <p>All leaderboard entries should be links to user profile page.</p>
-    </div>
-  </>)
-
+interface LeaderboardProps {
+  leaders: UserDetails[];
 }
 
+const Leaderboard = ({ leaders }: LeaderboardProps) => {
+  const [hydration, setHydration] = useState(false);
 
-export default Leaderboard
+  useEffect(() => {
+    setHydration(true);
+  }, []);
+
+  return (
+    <div className='w-full h-80 flex flex-row flex-wrap items-center justify-center text-gray-900'>
+      {hydration &&
+        leaders.map((leader: UserDetails) => (
+          <Leader key={leader.username} leader={leader} />
+        ))}
+    </div>
+  );
+};
+
+const getLeaderboardUsers = async () => {
+  const response = await fetch(checkEnvironment().concat('/api/leaderboard'), {
+    next: { revalidate: 20 },
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch');
+  }
+  return response.json();
+};
+
+export async function getStaticProps() {
+  const leaderboard: LeaderboardData = await getLeaderboardUsers();
+  const leaders = leaderboard?.leaderboard;
+
+  return {
+    props: {
+      leaders,
+    },
+  };
+}
+
+export default Leaderboard;
