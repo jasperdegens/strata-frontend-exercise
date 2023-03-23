ea54ce6928f5a5196d514f20f8708b8a788e9675
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-03-23 11:37:51 +0900

    Created Leaderboard page

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,19 +1,30 @@
-import { FC } from "react"
+// React
+import { FC, useContext } from 'react'
 
+// Types
+import { UserDetails } from '../../types'
 
+// Components
+import UserCard from '../../components/UserCard'
+
+// Utils
+import MainContext from '../../utils/contexts/MainContext'
 
 const Leaderboard: FC = () => {
+  const { allUsers } = useContext(MainContext)
 
-
-  return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">Leaderboard</h1>
-      <p>TODO: complete.</p>
-      <p>All leaderboard entries should be links to user profile page.</p>
-    </div>
-  </>)
-
+  return (
+    <>
+      <div className="w-full flex flex-col items-center justify-center text-black">
+        <ul>
+          {allUsers &&
+            allUsers.map((user: UserDetails) => (
+              <UserCard user={user} key={user.username} />
+            ))}
+        </ul>
+      </div>
+    </>
+  )
 }
-
 
 export default Leaderboard
