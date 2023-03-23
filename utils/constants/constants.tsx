a25fa87b76439336986854b1d11b137f5e528353
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-03-23 14:48:02 +0900

    Fixed constants to attempt deployment

--- utils/constants/constants.tsx
+++ utils/constants/constants.tsx
@@ -1,10 +1,7 @@
-export const HOST_URL =
-  process.env.NEXT_PUBLIC_HOST_URL || 'http://localhost:3002'
+export const API_LEADERBOARD_URL = '/api/leaderboard'
+export const API_PROFILE_URL = '/api/profile'
 
-export const API_LEADERBOARD_URL = `${HOST_URL}/api/leaderboard`
-export const API_PROFILE_URL = `${HOST_URL}/api/profile`
-
-export const LEADERBOARD_URL = `${HOST_URL}/leaderboard`
-export const PROFILE_URL = `${HOST_URL}/profile`
+export const LEADERBOARD_URL = '/leaderboard'
+export const PROFILE_URL = '/profile'
 
 export const INTERVAL_VALUE = 20000
