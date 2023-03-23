3635c27352bc02a67b2b0f12b94553caf8845987
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-03-23 11:03:54 +0900

    Created useFetch Hook

---
+++ utils/hooks/useFetch.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+export const useFetch = (url: URL) => {
+  const [data, setData] = useState({ leaderboard: [] })
+  const [error, setError] = useState<unknown>(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const response = await (await fetch(url)).json()
+        setData(response)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return { data, loading, error }
+}
