93733c0373073d2ab11527343734738614547515
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-03-23 13:48:01 +0900

    Changed Host for deployment

--- utils/constants/constants.tsx
+++ utils/constants/constants.tsx
@@ -1,4 +1,5 @@
-export const HOST_URL = 'http://localhost:3002'
+export const HOST_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 
 export const API_LEADERBOARD_URL = `${HOST_URL}/api/leaderboard`
 export const API_PROFILE_URL = `${HOST_URL}/api/profile`
