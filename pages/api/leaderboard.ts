45a58ef848f642172f4b43d50ad9bef66ff054f3
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-03-23 14:14:15 +0900

    Fixed data imports errors in API for deployment

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,27 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import fs from "fs";
-import path from "path";
+import fs from 'fs'
+import path from 'path'
+
+// Types
+import { LeaderboardData } from '../../types'
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<LeaderboardData>
 ) {
   // read all files from public leaderboard folder
-  const imgPaths = fs.readdirSync(path.join(process.cwd(), "public", "users"));
+  const imgPaths = fs.readdirSync(path.join(process.cwd(), 'public', 'users'))
 
   // randomize images
-  imgPaths.sort(() => Math.random() - 0.5);
+  imgPaths.sort(() => Math.random() - 0.5)
 
   const leaderboard = imgPaths.map((imgPath) => {
     return {
       // trim extensions from filenames
-      username: imgPath.replace(/\.[^/.]+$/, ""),
+      username: imgPath.replace(/\.[^/.]+$/, ''),
       profileImage: `/users/${imgPath}`,
       score: 100 + Math.floor(Math.random() * 1000),
-    };
-  });
+      favorite: false,
+    }
+  })
 
-  res.status(200).json({ leaderboard });
+  res.status(200).json({ leaderboard })
 }
